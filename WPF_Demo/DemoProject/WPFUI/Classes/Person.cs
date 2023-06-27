c205370929e5b278fed56c4a10989532010832ea
Roypc86 <roypc86@gmail.com>
2023-06-27 08:33:57 -0600

    Tutorial 2 adding new elements.

---
+++ WPF_Demo/DemoProject/WPFUI/Classes/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace WPFUI
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+    }
+}
---
+++ WPF_Demo/DemoProject/WPFUI/Classes/PersonManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.Classes
+{
+    class PersonManager
+    {
+        public List<Person> GetPeople()
+        {
+            List<Person> person_list = new List<Person>();
+            person_list.Add(new Person { FirstName = "Adam", LastName = "Figeres" });
+            person_list.Add(new Person { FirstName = "Tom", LastName = "Pas" });
+            person_list.Add(new Person { FirstName = "Charles", LastName = "Hemsworth" });
+            return person_list;
+        }
+    }
+}
