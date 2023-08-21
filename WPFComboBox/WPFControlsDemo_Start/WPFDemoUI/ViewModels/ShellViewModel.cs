a9624e6eccd942b2b7e4d2bf00c478cc4884fd7f
GreivinCalderon <greivin.calderon@efinancial.com>
2023-08-21 10:45:15 -0600

    Test commit

--- WPFComboBox/WPFControlsDemo_Start/WPFDemoUI/ViewModels/ShellViewModel.cs
+++ WPFComboBox/WPFControlsDemo_Start/WPFDemoUI/ViewModels/ShellViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using Caliburn.Micro;
 using DemoLibrary;
 using DemoLibrary.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WPFDemoUI.ViewModels
 {
